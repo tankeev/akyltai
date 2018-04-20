85783ff5e0da5a623babcaaf74af7948edc849f8
unknown <tankeev@gmail.com>
2018-04-20 01:10:39 -0700

    chages to be added for static settings

--- akyltai/wsgi.py
+++ akyltai/wsgi.py
@@ -19,3 +19,12 @@ application = get_wsgi_application()
 
 from whitenoise.django import DjangoWhiteNoise
 application = DjangoWhiteNoise(application)
+
+import os
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "akyltai.settings")
+
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)

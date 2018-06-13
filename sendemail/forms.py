efe6399fd47a609c07d9a1fa82e00eaac29929d2
unknown <tankeev@gmail.com>
2018-06-13 03:45:03 -0700

    mobile menu

--- sendemail/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-class ContactForm(forms.Form):
-    name = forms.CharField(required=True)
-    surname = forms.CharField(required=True)
-    from_email = forms.EmailField(required=False)
-    phone = forms.IntegerField(required=False)
-    message = forms.CharField(widget=forms.Textarea, required=True)
--- sendemail/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.conf.urls import include, url
-
-from . import views
-
-urlpatterns = [
-    url(r'^contact/', views.emailView, name='contact'),
-    url('success/', views.successView, name='success'),
-]
